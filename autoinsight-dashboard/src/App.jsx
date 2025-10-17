7d6b4234fb13752504d5541c9d4127b0fd341a5b
Shreeharsh <info.shreeharshshivpuje@gmail.com>
2025-10-17 14:23:43 +0530

    Created Data Manupulation Page and Univaral Page For Testing

--- autoinsight-dashboard/src/App.jsx
+++ autoinsight-dashboard/src/App.jsx
@@ -19,7 +19,7 @@ import TestFileSource from './pages/FileStorage/TestFileSource';
 import TestApiConnection from './pages/ApiEndpointsConnections/TestApiConnection';
 import MySQLConfiguration from './pages/Mysql/MySQLConfiguration';
 import TestMySQLConnection from './pages/Mysql/TestMySQL';
-import DataExplore from './pages/DataExplore/dataExplore.jsx';
+import DataExplore from './pages/DataExplore/dataExplore';
 import DataManipulation from './pages/DataManipulation/DataManipulation';
 import TestingPage from './pages/Testing/TestingPage';
 import './App.css';
